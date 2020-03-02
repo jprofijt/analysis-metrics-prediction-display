dfe7df97fbdb7c4e28fe5a1f122a6e4c6ab9a627
jprofijt <jouke.profijt@protonmail.com>
2020-03-02 10:49:50 +0100

    Made summary table parser pep8 compliant

--- src/scripts/python/ParseSummaryTable.py
+++ src/scripts/python/ParseSummaryTable.py
@@ -2,6 +2,7 @@ from typing import List, AnyStr
 import csv
 import sys
 
+
 class IncompatibleRowException(Exception):
     """Error to throw when rows are incompatible"""
     def __init__(self, message):
@@ -13,47 +14,65 @@ class SummaryTable:
     def __init__(self, headerRow: List):
         self.headerLength = len(headerRow)
         self.matrix = [headerRow]
-    
+
     def getHeader(self):
         return ", ".join(self.matrix[0])
-    
+
     def addRow(self, row: List):
         if len(row) != self.headerLength:
-            raise IncompatibleRowException(f'Length of row not { self.headerLength }')
+            raise \
+                IncompatibleRowException(
+                    f'Length of row not { self.headerLength }'
+                    )
         else:
             self.matrix.append(row)
-    
+
     def toCSV(self, output: AnyStr):
         with open(output, 'w', newline='') as file:
             csv.writer(file).writerows(self.matrix)
-            
-    
-                
-            
-    
-        
-with open('data/SummaryTable.csv') as csvFile:
-    SummaryTableReader = csv.reader(csvFile, delimiter=",")
-    for row in SummaryTableReader:
-        joint_row = ", ".join(row)
-        if len(row) == 0 or joint_row.startswith('#') or (joint_row.startswith("Extracted") or joint_row.startswith("Called") or joint_row.startswith("Scored")):
-            continue
-        elif len(row) == 1:
-            try:
-                currentTable.toCSV(f'{currentTableID}.csv')
-            except NameError:
-                pass
-            currentTableID = joint_row
-            lineCount = 0
-        elif len(row) > 1 and lineCount == 0:
-            currentTable = SummaryTable(row)
-            lineCount += 1
-        else:
-            try:
-                currentTable.addRow(row)
-            except IncompatibleRowException:
-                print(f'Something went wrong while parsing the table {currentTableID}')
-                print(f'Header: {currentTable.getHeader()} incompatible with: {joint_row}')
-                sys.exit("Quitting Program due to parsing error")
 
+def main():
+    with open('data/SummaryTable.csv') as csvFile:
+        SummaryTableReader = csv.reader(csvFile, delimiter=",")
+        lineCount = 0
+        for row in SummaryTableReader:
+            joint_row = ", ".join(row)
+            lineAtStartorEnd = (
+                joint_row.startswith('#')
+                or joint_row.startswith("Extracted")
+                or joint_row.startswith("Called")
+                or joint_row.startswith("Scored")
+                )
 
+            if len(row) == 0 or lineAtStartorEnd:
+                continue
+
+            elif len(row) == 1:
+                try:
+                    currentTable.toCSV(f'{currentTableID}.csv')
+                except NameError:
+                    pass
+                currentTableID = joint_row
+                lineCount = 0
+            elif len(row) > 1 and lineCount == 0:
+                currentTable = SummaryTable(row)
+                lineCount += 1
+            else:
+                try:
+                    currentTable.addRow(row)
+                except IncompatibleRowException:
+                    print(
+                        """
+                        Something went wrong while parsing the table {0}
+                        Header: {1} incompatible with: {2}
+                        """.format(
+                            currentTableID,
+                            currentTable.getHeader(),
+                            joint_row
+                        ))
+
+                    return 1
+        return 0
+
+if __name__ == "__main__":
+    main()
