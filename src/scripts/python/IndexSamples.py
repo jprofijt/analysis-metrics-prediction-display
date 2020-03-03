4b462664997349c05a6d85615a531398da07a2c2
jprofijt <jouke.profijt@protonmail.com>
2020-03-03 11:14:15 +0100

    made input and output required arguments

--- src/scripts/python/IndexSamples.py
+++ src/scripts/python/IndexSamples.py
@@ -95,8 +95,8 @@ def parseArguments():
     :rtype: class Files
     """
     parser = argparse.ArgumentParser(description=u'Parses sample sheet and adds to output file')
-    parser.add_argument(u'-i', u'--input', type=unicode, help=u'Sample sheet to read')
-    parser.add_argument(u'-o', u'--output', type=unicode, help=u'file to add results to')
+    parser.add_argument(u'-i', u'--input', type=unicode,metavar="path/to/input", help=u'Sample sheet to read', required=True)
+    parser.add_argument(u'-o', u'--output', type=unicode,metavar="path/to/output", help=u'file to add results to', required=True)
     args = parser.parse_args()
     files = Files(args.input, args.output)
     return files
