f714ba0b1a1939a6a06383267d4b3a4f5a452488
jprofijt <jouke.profijt@protonmail.com>
2020-06-02 15:49:35 +0200

    Added docstring to database type interface

--- src/scripts/python/QualityParser/DataTypes/DatabaseType.py
+++ src/scripts/python/QualityParser/DataTypes/DatabaseType.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python2.7
 
 class DatabaseType(object):
+    """Each Database type must have a toDatabase entry function which formats the data to the database format"""
     def toDatabaseEntry(self):
+         """Should format the information stored in the database type to an actual database type
+
+            Returns:
+            Database entry (Tuple): Tuple of values to add to database
+
+        """
         raise NotImplementedError
 
